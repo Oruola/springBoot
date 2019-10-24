17b8199a8923d39b8f54f55a2a0b199c636a1c4d
admin <1300266575@qq.com>
2019-10-25 00:11:03 +0800

    springboot启动类的提交

---
+++ src/main/java/cn/itcast/demo/MySpringBootAppliaction.java
@@ -0,0 +1,14 @@
+package cn.itcast.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 该类的作用:
+ */
+@SpringBootApplication
+public class MySpringBootAppliaction {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootAppliaction.class);
+    }
+}
